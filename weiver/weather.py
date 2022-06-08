a5a5cd39b22b49666c64037bf47f65341fb4b281
Vladimirov Kirill <kvladimirov.main@gmail.com>
2022-06-09 02:14:59 +0300

    add weatherapi

---
+++ weiver/weather.py
@@ -0,0 +1,13 @@
+from weiver.utils.gps import get_gps_coordinates
+from weiver.api_service import get_weather
+from weiver.utils.weather_formater import format_weather
+
+
+def main():
+    coordinates = get_gps_coordinates()
+    weather = get_weather(coordinates)
+    print(format_weather(weather))
+
+
+if __name__ == "__main__":
+    main()
