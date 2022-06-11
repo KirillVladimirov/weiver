b2e0c982f1d864888e8ce02d87c35fd8a2095dda
Vladimirov Kirill <kvladimirov.main@gmail.com>
2022-06-11 03:48:08 +0300

    change storage

--- weiver/utils/history.py
+++ weiver/utils/history.py
@@ -1,7 +1,14 @@
+import json
 from datetime import datetime
 from pathlib import Path
 from weiver.api_service import Weather
 from weiver.utils.weather_formater import format_weather
+from typing import TypedDict
+
+
+class HistoryRecord(TypedDict):
+    date: str
+    weather: str
 
 
 class WeatherStorage:
@@ -18,6 +25,32 @@ class PlainFileWeatherStorage(WeatherStorage):
         formatted_weather = format_weather(weather)
         with open(self._file, 'a') as f:
             f.write(f"{now}\n{formatted_weather}\n\n")
+
+
+class JSONFileWeatherStorage(WeatherStorage):
+    def __init__(self, file: Path):
+        self._file = file
+        self._init_storage()
+
+    def save(self, weather: Weather) -> None:
+        history = self._read_history()
+        history.append({
+            "date": str(datetime.now()),
+            "weather": format_weather(weather)
+        })
+        self._write(history)
+
+    def _init_storage(self) -> None:
+        if not self._file.exists():
+            self._file.write_text("[]")
+
+    def _read_history(self) -> list[HistoryRecord]:
+        with open(self._file, "r") as f:
+            return json.load(f)
+
+    def _write(self, history: list[HistoryRecord]) -> None:
+        with open(self._file, "w") as f:
+            json.dump(history, f, ensure_ascii=False, indent=4)
 
 
 def save_weather(weather: Weather, storage: WeatherStorage) -> None:
--- weiver/weather.py
+++ weiver/weather.py
@@ -2,7 +2,7 @@ from weiver.utils.gps import get_coordinates
 from weiver.api_service import get_weather
 from weiver.utils.weather_formater import format_weather
 from weiver.exceptions import CantGetCoordinates, ApiServiceError
-from weiver.utils.history import save_weather, PlainFileWeatherStorage
+from weiver.utils.history import save_weather, JSONFileWeatherStorage
 from pathlib import Path
 
 
@@ -18,7 +18,7 @@ def main():
         print("Cant get weather from the service.")
         exit(1)
     print(format_weather(weather))
-    save_weather(weather, PlainFileWeatherStorage(Path.cwd() / "history.txt"))
+    save_weather(weather, JSONFileWeatherStorage(Path.cwd() / "history.json"))
 
 
 if __name__ == "__main__":
